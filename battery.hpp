415b78393ce80b49305b691b8d2b85942dba6cf2
vphilippoz <82140967+vphilippoz@users.noreply.github.com>
2024-05-31 15:23:11 +0200

    Updated battery module with pinout

--- battery.hpp
+++ battery.hpp
@@ -2,11 +2,7 @@
 #define BATTERY_H
 
 #include <Arduino.h>
-
-#define VBAT_PIN A1
-#define BATSELA_PIN 6
-#define BATSELB_PIN 7
-#define BATSELC_PIN 8
+#include "pinout.hpp"
 
 // Function prototypes
 void battery_setup();
