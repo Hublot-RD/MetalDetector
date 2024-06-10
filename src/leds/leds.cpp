ec40ce7b7456506a5e3da7bb398ba93b0fe43f24
vphilippoz <82140967+vphilippoz@users.noreply.github.com>
2024-06-10 12:05:14 +0200

    LEDs dimmed to avoid getting blind

--- src/leds/leds.cpp
+++ src/leds/leds.cpp
@@ -2,7 +2,8 @@
 
 namespace leds {
 
-constexpr uint8_t NUM_LEDS = 8;
+constexpr uint8_t NUM_LEDS = 8;         // Number of LEDs
+constexpr uint8_t BRIGHTNESS = 100;     // LEDs brightness, 0-255
 CRGBArray<NUM_LEDS> led_stick;
 
 void setup(void) {
@@ -18,7 +19,7 @@ void set(uint8_t channel, CRGB color) {
 void animate(void) {
     for (uint8_t hue = 0; hue < 255; hue++) {
         for(uint8_t i = 0; i < NUM_LEDS; i++) {
-            led_stick[i] = CHSV(hue+32*i, 255, 255);
+            led_stick[i] = CHSV(hue+32*i, 255, BRIGHTNESS);
         }
         FastLED.show();
         delay(10);
